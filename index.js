28bbfaaa212e918c3a6f0208dc6919ebe165778d
Ivan Afanasev <diykarelia@gmail.com>
2023-10-18 16:49:29 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
 const fs = require('fs');
 const { parse } = require('csv');
 
-const fileName = './input.csv';
-const records = [];
-const domains = [];
-let uniqueDomains;
-let uniqueDomainsArr;
+const inputFilePath = './input.csv';
+const outputFilePath = './output.csv';
 
-const parser = parse({
-	delimiter: ',',
-});
+const parseEmailDomains = () => {
+	const records = [];
+	const domains = [];
 
-// reading and parsing email domains from csv file
-parser.on('readable', function () {
-	let record;
-	while ((record = parser.read()) !== null) {
-		records.push(record);
-	}
-});
-
-parser.on('error', function (err) {
-	console.error(err.message);
-});
-
-parser.on('end', function () {
-	records.forEach((record) => {
-		const email = record[0];
-		const domain = email.split('@').pop();
-		domains.push(domain);
-		// removing duplicate domains
-		uniqueDomains = Array.from(new Set(domains));
+	const parser = parse({
+		delimiter: ',',
 	});
 
-	// writing domains array to a new csv file
-	const csvContent = uniqueDomains.join('\n');
-
-	fs.writeFile('output.csv', csvContent, function (err) {
-		if (err) throw err;
-		console.log('The output file has been created!');
+	parser.on('readable', function () {
+		let record;
+		while ((record = parser.read()) !== null) {
+			records.push(record);
+		}
 	});
-});
 
-fs.createReadStream(fileName).pipe(parser);
+	parser.on('error', function (err) {
+		console.error(`Error while parsing input file: ${err.message}`);
+	});
+
+	parser.on('end', function () {
+		records.forEach((record) => {
+			const email = record[0];
+			const domain = email.split('@').pop();
+			domains.push(domain);
+		});
+
+		const uniqueDomains = Array.from(new Set(domains));
+		const csvContent = uniqueDomains.join('\n');
+
+		fs.writeFile(outputFilePath, csvContent, function (err) {
+			if (err) {
+				console.error(`Error while writing output file: ${err.message}`);
+				return;
+			}
+
+			console.log('The output file has been created!');
+		});
+	});
+
+	fs.createReadStream(inputFilePath).pipe(parser);
+};
+
+parseEmailDomains();
